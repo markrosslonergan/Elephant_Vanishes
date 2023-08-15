cb983f940e6883b504bd5b34dc4bc4b312258a82
markrosslonergan <markrosslonergan@gmail.com>
2023-08-15 10:34:44 -0500

    Starting Aug retreat

--- bin/PROmfa4u.cxx
+++ bin/PROmfa4u.cxx
@@ -84,13 +84,13 @@ struct DomainArgs
     vector<int>         starts;                     // starting offsets of ndom_pts (optional, usually assumed 0)
     vector<int>         ndom_pts;                   // number of points in domain (possibly a subset of full domain)
     vector<int>         full_dom_pts;               // number of points in full domain in case a subset is taken
-    vector<float>      min;                        // minimum corner of domain
-    vector<float>      max;                        // maximum corner of domain
-    vector<float>      s;                          // scaling factor for each variable or any other usage
-    float              r;                          // x-y rotation of domain or any other usage
-    vector<float>      f;                          // frequency multiplier for each variable or any other usage
-    float              t;                          // waviness of domain edges or any other usage
-    float              n;                          // noise factor [0.0 - 1.0]
+    vector<double>      min;                        // minimum corner of domain
+    vector<double>      max;                        // maximum corner of domain
+    vector<double>      s;                          // scaling factor for each variable or any other usage
+    double              r;                          // x-y rotation of domain or any other usage
+    vector<double>      f;                          // frequency multiplier for each variable or any other usage
+    double              t;                          // waviness of domain edges or any other usage
+    double              n;                          // noise factor [0.0 - 1.0]
     string              infile;                     // input filename
     bool                multiblock;                 // multiblock domain, get bounds from block
     bool                structured;                 // input data lies on unstructured grid
@@ -139,7 +139,7 @@ struct Block : public BlockBase<T>
         void load(void* b_, diy::BinaryBuffer& bb)          { mfa::load<Block, T>(b_, bb); }
 
 
-    // read a floating point 2d scalar dataset from HDF5
+    // read a doubleing point 2d scalar dataset from HDF5
     // reads masses for geometry dimension 0 from same HDF5 file
     // assigns integer values for the geometry dimension 1 from 0 to n_pts - 1
     // f = (mass, y, value)
@@ -169,9 +169,12 @@ struct Block : public BlockBase<T>
         // Create input data set and add to block
         std::cout << "dom_dim, mdims, ndom_pts.prod(), ndom_pts: " << dom_dim << ", " << mdims << ", " << ndom_pts.prod() << ", " << ndom_pts << std::endl;
         input = new mfa::PointSet<T>(dom_dim, mdims, ndom_pts.prod(), ndom_pts);
-        assert(vals(0) == ndom_pts(0));
-        assert(vals(1) == ndom_pts(1));
-        assert(vals(2) == ndom_pts(2));
+        std::cout<<" vals(0): \n "<<vals(0)<<" \n ndom_pts(0): \n "<<ndom_pts(0)<<std::endl;
+        std::cout<<vals(1)<<" "<<ndom_pts(1)<<std::endl;
+        std::cout<<vals(2)<<" "<<ndom_pts(2)<<std::endl;
+        //assert(vals(0) == ndom_pts(0));
+        //assert(vals(1) == ndom_pts(1));
+        //assert(vals(2) == ndom_pts(2));
         // set geometry values
         int n = 0;
         int pd = mfa_info.pt_dim();
@@ -182,9 +185,9 @@ struct Block : public BlockBase<T>
                     input->domain(n, 1) = j;
                     input->domain(n, 2) = k;
                     for (int m = 0; m < pd-dom_dim; m++) {
-                        //std::cout << "index n, 3+m, i, j, k, vals: " << n << ", " << 3+m << ", " << i << ", " << j << ", " << k << ", ";
+                        std::cout << "index n, 3+m, i, j, k, vals: " << n << ", " << 3+m << ", " << i << ", " << j << ", " << k << ", ";
                         input->domain(n, dom_dim+m) = vals(m, i, j, k);
-                        //std::cout << vals(m, i, j, k) << std::endl;
+                        std::cout << vals(m, i, j, k) << std::endl;
 
                     }	
                     n++;
@@ -212,8 +215,10 @@ struct Block : public BlockBase<T>
         core_mins = bounds_mins.head(dom_dim);
         core_maxs = bounds_maxs.head(dom_dim);
 
-        //std::cout << "tot_ndom_pt, input->domain(tot_ndom_pts - 1, 1), this->dom_dim = " << tot_ndom_pts << ", " << input->domain(tot_ndom_pts - 1, 1) << ", " << this->dom_dim << std::endl;
-
+        std::cout<<"Core Mins: "<<core_mins<<std::endl;
+        std::cout<<"Core Maxs: "<<core_maxs<<std::endl;
+        std::cout<<"Bound Mins: "<<bounds_mins<<std::endl;
+        std::cout<<"Bound Maxs: "<<bounds_maxs<<std::endl;
         // debug
         //cerr << "domain extent:\n min\n" << this->bounds_mins << "\nmax\n" << this->bounds_maxs << endl;
     }
@@ -221,22 +226,24 @@ struct Block : public BlockBase<T>
 };
 
 
-inline void makeSignalModel(diy::mpi::communicator world, Block<Real_t>* b, const diy::Master::ProxyWithLink& cp, int nbins, int mfadim, std::vector<int> nctrl_pts, int deg)
+inline void makeSignalModel(diy::mpi::communicator world, Block<double>* b, const diy::Master::ProxyWithLink& cp, int nbins, int mfadim, std::vector<int> nctrl_pts, int deg)
 {
     // default command line arguments
     int    dom_dim      = mfadim;                    // dimension of domain (<= pt_dim)
     int    pt_dim       = nbins+dom_dim;        // dimension of input points
-    Real_t noise        = 0.0;                  // fraction of noise
+    double noise        = 0.0;                  // fraction of noise
 
     vector<int> v_degrees(dom_dim, deg);
     vector<int> v_nctrls = nctrl_pts;
 
+    std::cout<<"start to construct ModelInfo"<<std::endl;
     // Info classes for construction of MFA
     ModelInfo geom_info(dom_dim);
     ModelInfo var_info(dom_dim, pt_dim - dom_dim, v_degrees, v_nctrls);
     MFAInfo   mfa_info(dom_dim, 1, geom_info, var_info);
     mfa_info.weighted = 0;
-    
+   
+    std::cout<<"Settting DomainArgs"<<std::endl;
     // set input domain arguments
     DomainArgs d_args(dom_dim, pt_dim);
     d_args.n            = noise;
@@ -251,10 +258,10 @@ inline void makeSignalModel(diy::mpi::communicator world, Block<Real_t>* b, cons
     Eigen::VectorXd vec_gridz(v_nctrls[2]);
 
     //tmp
-    int signalgridsize = 10;
     int mass = v_nctrls[0];
     int dim2 = v_nctrls[1];
     int dim3 = v_nctrls[2];
+    int signalgridsize = mass*dim2*dim3;
 
     Eigen::Tensor<double, 4> map_bin_to_grid(nbins,mass,dim2,dim3);
     Eigen::ArrayXXd values(signalgridsize,nbins);	
@@ -267,7 +274,9 @@ inline void makeSignalModel(diy::mpi::communicator world, Block<Real_t>* b, cons
     double t0 = MPI_Wtime();
     for (size_t i=0; i<signalgridsize; ++i) {
         double t_a = MPI_Wtime();
-        values.row(i) = 10; //signal.predict3D(world, cp, i, false, gridx, gridy, gridz);
+        Eigen::VectorXd res(nbins);
+        res.setRandom();
+        values.row(i) = res;//signal.predict3D(world, cp, i, false, gridx, gridy, gridz);
         double t_b = MPI_Wtime();
         int gridz_index = i / (dim2 * mass);
         int gridy_index = (i / mass) % dim2;
@@ -341,15 +350,12 @@ int main(int argc, char* argv[])
     int mfadim = 3; //Dimension of the MFA model
     int nBins = 1; //Dimension of output of MFA? I believe so
     int degree = 2;//Science degree? Not sure
-    int mass = 10;//number of mass pts
-    int dim2 = 10;//number of dim2 pts?
-    int dim3 = 1;
+    int mass = 20;//number of mass pts
+    int dim2 = 20;//number of dim2 pts?
+    int dim3 = 20;
     std::vector<int> ncontrol_pts = {mass,dim2,dim3};
 
-
-
-
-    Bounds<float> fc_domain(mfadim);
+    Bounds<double> fc_domain(mfadim);
     for(int i=0; i<mfadim; i++){
         fc_domain.min[i] = 0.;
         fc_domain.max[i] = blocks-1;
@@ -357,20 +363,22 @@ int main(int argc, char* argv[])
 
     diy::FileStorage               storage("./DIY.XXXXXX");
     diy::RoundRobinAssigner        fc_assigner(world.size(), blocks);
-    diy::RegularDecomposer<Bounds<float>> fc_decomposer(mfadim, fc_domain, blocks);
+    diy::RegularDecomposer<Bounds<double>> fc_decomposer(mfadim, fc_domain, blocks);
     diy::RegularBroadcastPartners  fc_comm(    fc_decomposer, mfadim, true);
     diy::RegularMergePartners      fc_partners(fc_decomposer, mfadim, true);
-    diy::Master                    fc_master(world, 1, -1, &Block<float>::create, &Block<float>::destroy, &storage, &Block<float>::save, &Block<float>::load);
+    diy::Master                    fc_master(world, 1, -1, &Block<double>::create, &Block<double>::destroy, &storage, &Block<double>::save, &Block<double>::load);
     diy::ContiguousAssigner   assigner(world.size(), blocks);
 
+        std::cout<<"fc_decomposer.decompose"<<std::endl;
         fc_decomposer.decompose(world.rank(),
             assigner,
-            [&](int gid, const Bounds<Real_t>& core, const Bounds<Real_t>& bounds, const Bounds<Real_t>& domain, const RCLink<Real_t>& link)
-            { Block<Real_t>::add(gid, core, bounds, domain, link, fc_master, mfadim, nBins+mfadim, 0.0); });
+            [&](int gid, const Bounds<double>& core, const Bounds<double>& bounds, const Bounds<double>& domain, const RCLink<double>& link)
+            { Block<double>::add(gid, core, bounds, domain, link, fc_master, mfadim, nBins+mfadim, 0.0); });
 
 
+        std::cout<<"fc_master.foreach"<<std::endl;
         double T10 = MPI_Wtime();
-        fc_master.foreach([world,nBins, mfadim, ncontrol_pts, degree](Block<Real_t>* b, const diy::Master::ProxyWithLink& cp){
+        fc_master.foreach([world,nBins, mfadim, ncontrol_pts, degree](Block<double>* b, const diy::Master::ProxyWithLink& cp){
                 makeSignalModel(world, b, cp,  nBins, mfadim, ncontrol_pts, degree);});
 
         double T11   = MPI_Wtime();
@@ -378,9 +386,23 @@ int main(int argc, char* argv[])
 
 
 
+        std::cout<<"Testing"<<std::endl;
+ 
+
+
+        fc_master.foreach([&](Block<double>* b, const diy::Master::ProxyWithLink& cp){
+            Eigen::VectorXd out_pt(b->pt_dim);
+            Eigen::VectorXd in_param(b->dom_dim);
+            std::cout<<" Inside: "<<b->pt_dim<<" "<<b->dom_dim<<std::endl;
+            in_param(0) = 0;
+            in_param(1) = 0;
+            in_param(2) = 0;
+            b->decode_point(cp, in_param, out_pt);
+            std::cout<<"Final Res: \n "<<out_pt<<std::endl;               
+               });
+
 
         std::cout<<" OK, things are setup. "<<std::endl;
-
 
         PROconfig myConf(xmlname);
 
